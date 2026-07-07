ca6407062321e9555b2b3a6501bd910f668ec197
Gunjot kaur <gunjotvirdi@gmail.com>
2026-07-07 13:08:50 +0530

    Add Maven project with Jenkinsfile

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class AppTest {
+    @Test
+    public void testAdd() {
+        assertEquals(5, App.add(2, 3));
+    }
+}
