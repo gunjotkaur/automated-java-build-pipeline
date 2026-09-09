c3ef2f652003b7bfc40468afb087ee6895fbcbb8
Gunjot kaur <gunjotvirdi@gmail.com>
2026-09-09 23:55:10 +0530

    Updated to multiply

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,11 @@
 package com.example;
 
 public class App {
-    public static int Add(int a, int b) {
-        return a + b;
+    public static int multiply(int a, int b) {
+        return a * b;
     }
 
     public static void main(String[] args) {
-        System.out.println("Sum: " + Add(5, 3));
+        System.out.println("Product: " + multiply(5, 3));
     }
 }
