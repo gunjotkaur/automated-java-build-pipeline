f603274f12ff132f31db2bfd38a34a6294bf3205
Gunjot kaur <gunjotvirdi@gmail.com>
2026-09-11 23:11:15 +0530

    testing2

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,11 @@
 package com.example;
 
 public class App {
-    public static int add(int a, int b) {
-        return a + b;
+    public static int subtract(int a, int b) {
+        return a - b;
     }
 
     public static void main(String[] args) {
-        System.out.println("add: " +add(5, 3));
+        System.out.println("Subtract: " + subtract(5, 3));
     }
 }
