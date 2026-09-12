0551c9defc81abf29885f6fc78a7836205465367
Gunjot kaur <gunjotvirdi@gmail.com>
2026-09-12 21:24:40 +0530

    changed multiplication to addition

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,11 @@
 package com.example;
 
 public class App {
-    public static int multiply(int a, int b) {
-        return a * b;
+    public static int add(int a, int b) {
+        return a + b;
     }
 
     public static void main(String[] args) {
-        System.out.println("Multiply: " + multiply(5, 3));
+        System.out.println("Add: " + add(5, 3));
     }
 }
