3d8acd69461e21db6a66994a9eb1d1ca5ab63038
Gunjot Kaur <gunjotvirdi@gmail.com>
2026-09-12 20:29:07 +0530

    Update Java application

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,11 +1,11 @@
 package com.example;
 
 public class App {
-    public static int subtract(int a, int b) {
-        return a - b;
+    public static int multiply(int a, int b) {
+        return a * b;
     }
 
     public static void main(String[] args) {
-        System.out.println("Subtract: " + subtract(5, 3));
+        System.out.println("mulitply: " + multiply(5, 3));
     }
 }
